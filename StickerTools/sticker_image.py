1272a33fc03d4432b90e4270d2f0a5c54cc5f89e
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-10-05 10:19:40 +0530

    Update sticker_image.py

--- StickerTools/sticker_image.py
+++ StickerTools/sticker_image.py
@@ -4,32 +4,55 @@ from pyrogram.types import Message
 from pyrogram import Client, filters
 
 
-@Client.on_message(filters.private & filters.incoming & (filters.sticker | filters.photo))
-async def sticker_image(_, msg: Message):
-    user_id = msg.from_user.id
-    message_id = msg.message_id
-    name_format = f"StarkBots_{user_id}_{message_id}"
-    if msg.photo:
-        message = await msg.reply("Converting...")
-        image = await msg.download(file_name=f"{name_format}.jpg")
-        await message.edit("Sending...")
-        im = Image.open(image).convert("RGB")
-        im.save(f"{name_format}.webp", "webp")
-        sticker = f"{name_format}.webp"
-        await msg.reply_sticker(sticker)
-        await message.delete()
-        os.remove(sticker)
-        os.remove(image)
-    elif msg.sticker.is_animated:
-        await msg.reply("Animated stickers are not supported !", quote=True)
-    else:
-        message = await msg.reply("Converting...")
-        sticker = await msg.download(file_name=f"{name_format}.webp")
-        await message.edit("Sending...")
-        im = Image.open(sticker).convert("RGB")
-        im.save(f"{name_format}.jpg", "jpeg")
-        image = f"{name_format}.jpg"
-        await msg.reply_photo(image)
-        await message.delete()
-        os.remove(image)
-        os.remove(sticker)
+@Bot.on_message(filters.private & filters.media)
+async def media_filter(bot, update):
+    try:
+        message = await update.reply_text(
+            text="Processing...",
+            quote=True,
+            disable_web_page_preview=True
+        )
+        media = await update.download()
+        await message.edit_text(
+            text="Downloading...",
+            disable_web_page_preview=True
+        )
+        response = uploadFile(media)
+        await message.edit_text(
+            text="Uploading...",
+            disable_web_page_preview=True
+        )
+        try:
+            os.remove(media)
+        except:
+            pass
+    except Exception as error:
+        await update.reply_text(
+            text=f"Error :- <code>{error}</code>",
+            quote=True,
+            disable_web_page_preview=True
+        )
+        return
+    text = f"**File Name:** {response['fileName']}" + "\n"
+    text += f"**Download Page:** {response['downloadPage']}" + "\n"
+    text += f"**Direct Download Link:** {response['directLink']}" + "\n"
+    text += f"**Info:** {response['info']}"
+    reply_markup = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(text="Open Link", url=response['directLink']),
+                InlineKeyboardButton(text="Share Link", url=f"https://telegram.me/share/url?url={response['directLink']}")
+            ],
+            [
+                InlineKeyboardButton(text="Join Updates Channel", url="https://telegram.me/FayasNoushad")
+            ]
+        ]
+    )
+    await message.edit_text(
+        text=text,
+        reply_markup=reply_markup,
+        disable_web_page_preview=True
+    )
+
+
+Bot.run()
